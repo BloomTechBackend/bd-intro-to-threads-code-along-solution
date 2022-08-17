b76092f4d483d52c3d28b963007d20d2927acee3
Frank Fella <frank.fella@lambdaschool.com>
2022-08-17 15:42:16 -0700

    FFella Push 08/17/2022 15:42:16

--- src/main/java/com/bloomtech/codealong/MagicEightBall/controller/MagicEightBallController.java
+++ src/main/java/com/bloomtech/codealong/MagicEightBall/controller/MagicEightBallController.java
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@RestController                        // This class contains RESTful controllers
+@RestController  // This class contains RESTful controllers
 public class MagicEightBallController {
 
     // Instantiate the Dao to be used to acquire data
     private MagicEightBallDao theEightBall = new MagicEightBallDao();
-    
+
     /**
      * Handle HTTP POST requests for responses to questions
      *        with the URL: /magic8Ball/ask
@@ -27,48 +28,40 @@ public class MagicEightBallController {
      *
      * @param theRequest - Will contain a JSON array of strings passed through the request body
      *                     each element in the array is a question to ask the Magic 8 Ball
-     * @return           - A List of Magic8BallResponse objects each one containing
-     *                     a question asked, the answer to the question and response time
+     * @return           - A List of Magic8BallResponse objects, sorted by ascending response time,
+     *                     each one containing a question asked, the answer to the question, response time,
+     *                     return code and processing message
      */
-    @PostMapping(value="magic8ball/ask")
-    public List<MagicEightBallResponse> getResponse(@RequestBody MagicEightBallRequest theRequest) throws InterruptedException {
+    @PostMapping(value="/magic8ball/ask")
+    public List<MagicEightBallResponse> getResponse(@RequestBody MagicEightBallRequest theRequest) throws InterruptedException, CloneNotSupportedException {
         // Log the request to the server log
         System.out.println("-".repeat(100));
         logRequest("Request received via HTTP POST URL: /magic8ball/ask with " + theRequest.getQuestions().size() + " questions");
 
-        long requestStartTime = System.currentTimeMillis();       // Remember the time at the start of request
+        long requestStartTime = System.currentTimeMillis();       // Record start time of process
 
         List<MagicEightBallResponse> answers = new ArrayList<>(); // Data store for responses
 
-        List<Thread> ask8BallThreads = new ArrayList<>();
-        List<AskThe8Ball> questionsFor8Ball = new ArrayList<>();
+        int questionNumber = 0;  // Used to enumerate questions from the input array as they are processed
 
-        for (String aQuestion : theRequest.getQuestions()) {                      // Loop through teh questions received
+        // Loop through the array of questions received with the request
+        for (String aQuestion : theRequest.getQuestions()) {
+            // Instantiate an object to interact with the Magic8Ball
+            AskThe8Ball theMagic8Ball = new AskThe8Ball(++questionNumber, aQuestion);
 
-            AskThe8Ball askThe8Ball = new AskThe8Ball(aQuestion);
-            questionsFor8Ball.add(askThe8Ball);
-
-            Thread aThread = new Thread(askThe8Ball);
-            ask8BallThreads.add(aThread);
-            aThread.start();
+            // ask the question and store answer in response
+            answers.add(theMagic8Ball.shakeAndTurnOver());
         }
-        // Now that all the Threads have been started, we need to wait for them to complete
-        // before we can copy the response to the list of responses we are returning
 
-        waitForThreadsToComplete(ask8BallThreads);
-
-        // Now that all Threads have completed, we can copy the repsonses of their askthe8Ball processes
-        // to the list of responses we are returning
-
-        for(AskThe8Ball an8BallInteration : questionsFor8Ball) {
-            answers.add(an8BallInteration.getTheResponse());
-        }
+        // Sort the responses in ascending responses times
+        Collections.sort(answers);   // Note: This will use the compareTo() method in the class of the object
 
         // Log the end of request processing to the server log
         logRequest("Request completed in " + (System.currentTimeMillis() - requestStartTime) + " milliseconds");
+
         // Return a List of response objects with the question asked, answer and processing time from the Magic Eight Ball
         return answers;
-        }
+        } // End of processing for HTTP POST for /magic8Ball/ask
 
     /**
      * Handle HTTP Get requests for responses to questions
@@ -81,29 +74,18 @@ public class MagicEightBallController {
      * @param question - Will contain the question to be asked as a query parameter
      * @return         - A Magic8BallResponse object
      */
-    @GetMapping (value="magic8ball/ask")
-    public MagicEightBallResponse getResponse(@RequestParam String question) throws InterruptedException {
+    @GetMapping (value="/magic8ball/ask")
+    public MagicEightBallResponse getResponse(@RequestParam String question) throws InterruptedException, CloneNotSupportedException {
 
         // Log the request to the server log
         logRequest("Request received via HTTP GET URL: /magic8ball/ask and question: " + question);
-        long startMilliseconds = System.currentTimeMillis();                // Remember the time at the start of request
 
-        MagicEightBallResponse theResponse = new MagicEightBallResponse();  // Data store for responses
-        theResponse.setQuestion(question);                                  // Copy the question to the response object
-        try {
-            theResponse.setAnswer(theEightBall.getResponse());                // Ask the question and store answer in response
-        } catch (InterruptedException e) {                                    // If exception occurs, handle it
-            e.printStackTrace();                                              //    by printing the stack trace
-            theResponse.setReturnCode(500);                                   //    set return code to indicate problem
-            theResponse.setProcessingMessage("Magic 8 Ball processing error");//    set error message to return
+        // Instantiate an object to interact with the Magic8Ball
+        AskThe8Ball theMagic8Ball = new AskThe8Ball(question);
 
-        }
-        // Calculate time to process response and store it in response object
-        theResponse.setTimeToRespondSeconds(System.currentTimeMillis() - startMilliseconds);
-
-        // Return response object with the question asked, answer and processing time from the Magic Eight Ball
-        return theResponse;
-    }
+        // Get an repsonse from the Magic8Ball and return it
+        return theMagic8Ball.shakeAndTurnOver();
+     }  // End of processing for HTTP GET for /magic8Ball/ask
 
     /*****************************************************************************************
      * Helper method to log a message provided via parameter with timestamp
@@ -116,58 +98,41 @@ public class MagicEightBallController {
         System.out.println(new Timestamp(datetime) + "\t--> " + message);
     } // End of logRequest() method
 
-    /******************************************************************************************
-     * Put any additional code required by the Code-Along here
-     *****************************************************************************************/
-
     /**
-     * Nested class for processing to be run on the thread
+     * Nested class for obtaining a response from the magnificent Magic 8 Ball
      */
-    final class AskThe8Ball implements Runnable {
-
-        MagicEightBallResponse theResponse = new MagicEightBallResponse();    //    Instantiate an object to hold question response
+    class AskThe8Ball {
+        //    Instantiate an object to hold question response
+        private MagicEightBallResponse theResponse = new MagicEightBallResponse();
 
         public AskThe8Ball(String question) {
-            this.theResponse.setQuestion(question);   //    Copy the question to the response object
+            this.theResponse.setQuestionNumber(1);   // Set the question number to 1
+            this.theResponse.setQuestion(question);  // Copy the question to the response object
+        }
+
+        public AskThe8Ball(int questionNumber, String question) {
+            this.theResponse.setQuestionNumber(questionNumber); //    Copy the question number to the response object
+            this.theResponse.setQuestion(question);             //    Copy the question to the response object
         }
 
         public MagicEightBallResponse getTheResponse() {
             return theResponse;
         }
 
-        public void run() {
-            long startMilliseconds = System.currentTimeMillis();;                 //    Remember when processing started
+        // Obtain a response from the Magic8Ball
+        public MagicEightBallResponse shakeAndTurnOver() throws InterruptedException, CloneNotSupportedException {
+            // Remember when processing started
+            long startMilliseconds = System.currentTimeMillis();
 
-            try {
-                theResponse.setAnswer(theEightBall.getResponse());                //    ask the question and store answer in response
-            } catch (InterruptedException e) {                                    //    if exception occurs, handle it
-                e.printStackTrace();                                              //       by printing the stack trace
-                theResponse.setReturnCode(500);                                   //       set return code to indicate problem
-                theResponse.setProcessingMessage("Magic 8 Ball processing error");//       set error message to return
-            }
+            //  get an answer and store in the response
+            theResponse.setAnswer(theEightBall.getResponse());                //    ask the question and store answer in response
+
             // Calculate time to process response and store it in response object
             theResponse.setTimeToRespondSeconds(System.currentTimeMillis()-startMilliseconds);
-            
-            return;  // Not required - coded in case we need to break here for debugging
-        }  // End of run() method
+
+            // defensive return the response from the Magic 8 Ball
+            return theResponse.clone();
+        }  // End of shakeAndTurnOver() method
     }  // End of AskThe8Ball class
+}  // End of MagicEightBallController Class
 
-    /**
-     * Helper method to wait for all Threads to complete before resuming
-     *
-     * Makes the thread calling this method wait until all passed in threads are done executing before proceeding.
-     *
-     * @param threads to wait on
-     * @throws InterruptedException
-     *
-     * the .join() method will wait until a Thread is complete before resuming execution
-     *
-     */
-    private void waitForThreadsToComplete(List<Thread> threads) throws InterruptedException {
-        // Go through the List of threads we started and wait for them all to complete
-        for (Thread thread : threads) {
-            thread.join(); // wait for the current Thread to complete before resuming processing
-        }
-    }  // End of waitForThreadsToComplete() method
-
-}  // End of controller class
